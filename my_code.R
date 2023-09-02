d1a93dd4b05b31e5fa3615b77678d874c98ef1cd
RosemaryMadaki <rosemarymadaki4real@gmail.com>
2023-09-02 09:00:06 -0400

    MORE EDITS

--- my_code.R
+++ my_code.R
@@ -197,25 +197,11 @@ ggplot(sodium3, aes(x = BP)) +
 
 
 
-#8 Applying renv
-kkkkk
- more stuff
+#8 Applying renv, after the first innit, always check status and snapshot when you use a new package and endeavor to update it and commit
 
-llllll
- more changesjjkkk
-#From Git pull
- From https://github.com/RosemaryMadaki/final-project
- 90b3952..8dcc6c4  main       -> origin/main
- hint: You have divergent branches and need to specify how to reconcile them.
- hint: You can do so by running one of the following commands sometime before
- hint: your next pull:
- 	hint:
- 	hint:   git config pull.rebase false  # merge
- hint:   git config pull.rebase true   # rebase
- hint:   git config pull.ff only       # fast-forward only
- hint:
- 	hint: You can replace "git config" with "git config --global" to set a default
- hint: preference for all repositories. You can also pass --rebase, --no-rebase,
- hint: or --ff-only on the command line to override the configured default per
- hint: invocation.
- fatal: Need to specify how to reconcile divergent branches.
+#ALWAYS MAKE CHANGES IN THE LOCAL REPO BFR COMMITING NOT DOING SO DIRECTLY FROM THE REPO
+ #Fixing those wahala dem
+ #git pull --no--ff
+ #git config --global pull.rebase true
+ #git pull origin --rebase
+
