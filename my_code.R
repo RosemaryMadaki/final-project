b13db443f50419cdea6c53f94b59a8e330c5b329
RosemaryMadaki <rosemarymadaki4real@gmail.com>
2023-09-02 08:26:09 -0400

    updates

--- my_code.R
+++ my_code.R
@@ -195,7 +195,24 @@ ggplot(sodium3, aes(x = BP)) +
 	geom_histogram() +
 	geom_vline(aes(xintercept = mean_BP), price_stats, color = "blue", linewidth = 6)
 
+
+
 #8 Applying renv
 kkkkk
  more stuff
-
+#From Git pull
+ From https://github.com/RosemaryMadaki/final-project
+ 90b3952..8dcc6c4  main       -> origin/main
+ hint: You have divergent branches and need to specify how to reconcile them.
+ hint: You can do so by running one of the following commands sometime before
+ hint: your next pull:
+ 	hint:
+ 	hint:   git config pull.rebase false  # merge
+ hint:   git config pull.rebase true   # rebase
+ hint:   git config pull.ff only       # fast-forward only
+ hint:
+ 	hint: You can replace "git config" with "git config --global" to set a default
+ hint: preference for all repositories. You can also pass --rebase, --no-rebase,
+ hint: or --ff-only on the command line to override the configured default per
+ hint: invocation.
+ fatal: Need to specify how to reconcile divergent branches.
