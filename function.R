2c351bed9587c6d835990c69cbb97fb25d55738f
RosemaryMadaki <rosemarymadaki4real@gmail.com>
2023-09-02 13:06:15 -0400

    new function

---
+++ function.R
@@ -0,0 +1,24 @@
+#		My new function
+
+#Step1i
+za <- c(40, 30, 10)
+
+#Step2i
+n <- length(za)
+
+#Step3i
+sum(za) / n-1
+
+
+#Step 4i
+new_d <- function(za) {
+	n <- length(y)
+	dd_val <- sum(za) / n-1
+	return(dd_val)
+}
+
+#Step5
+new_d(za = za)
+
+#Step 6
+new_d(za = c(2, 2, 3))
